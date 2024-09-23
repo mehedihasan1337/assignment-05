311ce9e91a896c8ed93fc93f3b641bd8c9b923d1
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2024-09-23 03:39:31 -0600

    added a all

--- assignment5.js
+++ assignment5.js
@@ -1,6 +1,6 @@
 document.getElementById('button-home')
-addEventListener('click', function(event){
-    // event.preventDefault();
+.addEventListener('click', function(event){
+    event.preventDefault() 
     window.location.href = '/home.html'
 })
 // document.getElementById('button-blog')
