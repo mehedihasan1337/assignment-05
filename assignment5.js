36b36adc3a462acf8eee5386494ac477653ea08f
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2024-09-23 09:00:13 -0600

    added an all file

--- assignment5.js
+++ assignment5.js
@@ -4,4 +4,9 @@ function homeButton(){
         window.location = '/home.html'
     
 }
+function indexButton(){
+    document.getElementById('index-button')
+        window.location = '/index.html'
+    
+}
 
