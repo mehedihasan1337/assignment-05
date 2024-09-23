14efd75c6b5a27ffecdfc31fa41791865f6d0e80
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2024-09-23 00:25:29 -0600

    added and all blogs

---
+++ assignment5.js
@@ -0,0 +1,10 @@
+document.getElementById('button-home')
+addEventListener('click', function(event){
+    event.preventDefault();
+    window.location.href = '/home.html'
+})
+// document.getElementById('button-blog')
+// addEventListener('click', function(event){
+//     event.preventDefault();
+//     window.location.href = '/index.html'
+// })
