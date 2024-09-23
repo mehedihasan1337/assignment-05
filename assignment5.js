b62e2547688eb1a0c52604084916c982f850653a
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2024-09-23 00:29:28 -0600

    added a home page

--- assignment5.js
+++ assignment5.js
@@ -1,7 +1,7 @@
 document.getElementById('button-home')
 addEventListener('click', function(event){
     event.preventDefault();
-    window.location.href = '/home.html'
+    window.location.href = './home.html'
 })
 // document.getElementById('button-blog')
 // addEventListener('click', function(event){
