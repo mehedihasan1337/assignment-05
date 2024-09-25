2537ddd60d0a40d83bf502aad56c83f53e6f5f6b
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2024-09-25 08:33:57 -0600

    added a all blog

---
+++ assignment5.js
@@ -0,0 +1,11 @@
+// donate-btn1 
+// step-1: add 
+ document.getElementById('donate-a1').addEventListener('click', function(event){
+    event.preventDefault()
+    console.log('add')
+     
+ })
+ 
+
+
+
